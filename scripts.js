7f350dc01d54a29be71e0a3a9d475306092789ae
luisahug <0356177@feevale.br>
2024-09-23 14:32:14 -0300

    Adicionado validação de operadores para nao se sobreporem e tecla backspace

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 const botoes = document.querySelectorAll("button");
 const tela = document.getElementById("tela");
+const operacoes = ["%", "/", "*", "-", "+"];
 
 botoes.forEach(button => {
     button.addEventListener("click", ()=> {
-        tela.value += button.value;
+        const botaoAtual = button.value;
+        const telaAtual = tela.value;
+
+        if(operacoes.includes(botaoAtual)){
+            if(operacoes.includes(telaAtual.slice(-1))){
+                tela.value = telaAtual.slice(0,-1) + botaoAtual;
+            }
+            else {
+                tela.value +=botaoAtual
+            }
+        } else {
+            tela.value += botaoAtual;
+        }
+
+        if(button.id == "backspace"){
+            tela.value = telaAtual.slice(0,-1);
+        }
     })
 })
